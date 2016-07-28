8776336add7df3ff01aa5789fd6bc1aa5daad2c3
wyt930927 <2269321782@qq.com>
2016-07-28 09:10:00 +0800

    none

--- www/js/activity.js
+++ www/js/activity.js
@@ -3,7 +3,7 @@
  */
 angular.module('ActCtrl', [])
   .controller('ActivityCtrl', function ($scope, $location, $ionicPopup) {
-    $scope.a0 = aui - active;
+    $scope.a0 = "aui-active";
     $scope.goactab = function (n) {
       for (var i = 0; i <= 6; i++) {
         var i = a + "i";
