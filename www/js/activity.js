8b0c4319e32dd05fbd06391f1cd6ddfb710bd386
EclipseXuLu <249048056@qq.com>
2016-08-24 17:58:06 +0800

    --V0.6.1.20160824_alpha --活动页面动态数据加载显示

--- www/js/activity.js
+++ www/js/activity.js
@@ -2,23 +2,32 @@
  * Created by Administrator on 2016/7/25.
  */
 angular.module('ActCtrl', [])
-  .controller('ActallCtrl', function ($scope, $ionicHistory,LocalStorage) {
+  .controller('ActallCtrl', function ($scope, $ionicHistory, LocalStorage) {
   })
-  .controller('ActivityCtrl', function ($scope, $state, $http,LocalStorage) {
+  .controller('ActivityCtrl', function ($scope, $state, $http, LocalStorage) {
+
+    var username = localStorage.getItem("username");
     $scope.actdetial = function (A) {
       LocalStorage.set("acthViewid", "act1");
       $state.go("actdetial", {actobj: A});
     }
-    $http.get('../data/activity/tabs.json').then(function (response) {
+    $http.get('http://localhost:8080/api/acttype/' + username).then(function (response) {
       if (response.data.status == 0) {
-        $scope.tabs = response.data.tabs;
+        $scope.tabs = response.data.result;
+      }
+    });
+
+    //加载“banner”
+    $http.get('http://localhost:8080/api/banner').then(function (response) {
+      if (response.data.status == 0) {
+        $scope.bannerlist = response.data.result;
       }
     });
 
     //默认加载“推荐”的activitylist
-    $http.get('../data/activity/activity-list.json').then(function (response) {
+    $http.get('http://localhost:8080/api/' + username + '/activity/0').then(function (response) {
       if (response.data.status == 0) {
-        $scope.activitylist = response.data.activitylist;
+        $scope.activitylist = response.data.result;
       }
     });
 
@@ -40,59 +49,72 @@ angular.module('ActCtrl', [])
     $scope.currentTab = '推荐';
     // var currentTab = '推荐'
     $scope.onClickTab = function (tab) {
-      $scope.currentTab = tab.title;
+      $scope.currentTab = tab.actTypeName;
       //切换TAB时请求该TAB对应的数据
 
-      switch ($scope.currentTab) {
-        case "推荐": {
-          $http.get('../data/activity/activity-list.json').then(function (response) {
-            if (response.data.status == 0) {
-              $scope.activitylist = response.data.activitylist;
-            }
-          });
-        }
+      var actTypeId = tab.actTypeId;
+      console.log(actTypeId);
 
-        case "体育": {
-          $http.get('../data/activity/tabs/activity-list-sports.json').then(function (response) {
-            if (response.data.status == 0 && response.data.category == tab.title) {
-              $scope.activitylist = response.data.activitylist;
-            }
-          });
+      /**
+       * 根据点击TAB动态加载数据
+       */
+      $http.get('http://localhost:8080/api/' + username + '/activity/' + actTypeId).then(function (response) {
+        if (response.data.status == 0) {
+          $scope.activitylist = response.data.result;
         }
+      });
 
-        case "旅游": {
-          $http.get('../data/activity/tabs/activity-list-trip.json').then(function (response) {
-            if (response.data.status == 0 && response.data.category == tab.title) {
-              $scope.activitylist = response.data.activitylist;
-            }
-          });
-        }
+      /*      switch ($scope.currentTab) {
+       case "推荐": {
+       $http.get('http://localhost:8080/api/' + username + '/activity/0').then(function (response) {
+       if (response.data.status == 0) {
+       $scope.activitylist = response.data.result;
+       }
+       });
+       }
 
-        case "明星": {
-          $http.get('../data/activity/tabs/activity-list-star.json').then(function (response) {
-            if (response.data.status == 0 && response.data.category == tab.title) {
-              $scope.activitylist = response.data.activitylist;
-            }
-          });
-        }
+       case "体育": {
+       $http.get('../data/activity/tabs/activity-list-sports.json').then(function (response) {
+       if (response.data.status == 0 && response.data.category == tab.title) {
+       $scope.activitylist = response.data.activitylist;
+       }
+       });
+       }
 
-        case "电影": {
-          $http.get('../data/activity/tabs/activity-list-movie.json').then(function (response) {
-            if (response.data.status == 0 && response.data.category == tab.title) {
-              $scope.activitylist = response.data.activitylist;
-            }
-          });
-        }
+       case "旅游": {
+       $http.get('../data/activity/tabs/activity-list-trip.json').then(function (response) {
+       if (response.data.status == 0 && response.data.category == tab.title) {
+       $scope.activitylist = response.data.activitylist;
+       }
+       });
+       }
 
-        case "恋爱": {
-          $http.get('../data/activity/tabs/activity-list-love.json').then(function (response) {
-            if (response.data.status == 0 && response.data.category == tab.title) {
-              $scope.activitylist = response.data.activitylist;
-            }
-          });
-        }
+       case "明星": {
+       $http.get('../data/activity/tabs/activity-list-star.json').then(function (response) {
+       if (response.data.status == 0 && response.data.category == tab.title) {
+       $scope.activitylist = response.data.activitylist;
+       }
+       });
+       }
 
-      }
+       case "电影": {
+       $http.get('../data/activity/tabs/activity-list-movie.json').then(function (response) {
+       if (response.data.status == 0 && response.data.category == tab.title) {
+       $scope.activitylist = response.data.activitylist;
+       }
+       });
+       }
+
+       case "恋爱": {
+       $http.get('../data/activity/tabs/activity-list-love.json').then(function (response) {
+       if (response.data.status == 0 && response.data.category == tab.title) {
+       $scope.activitylist = response.data.activitylist;
+       }
+       });
+       }
+       }*/
+
+
     }
     $scope.isActivetab = function (tabUrl) {
       return tabUrl == $scope.currentTab;
@@ -105,7 +127,7 @@ angular.module('ActCtrl', [])
 
 
   /*活动详情*/
-  .controller('actdetialCtrl', function ($scope, $stateParams,$state, $http,$ionicHistory,LocalStorage) {
+  .controller('actdetialCtrl', function ($scope, $stateParams, $state, $http, $ionicHistory, LocalStorage) {
     console.log($ionicHistory.viewHistory())
     if ($stateParams.actobj != null) {
       $scope.actobj = $stateParams.actobj;
@@ -119,9 +141,9 @@ angular.module('ActCtrl', [])
       $scope.showcom = true;
     }
     $scope.gohBack = function () {
-      if(LocalStorage.get("acthViewid")=="act1"){
+      if (LocalStorage.get("acthViewid") == "act1") {
         $state.go("tab.activity")
-      }else{
+      } else {
         var backHistoryId = $ionicHistory.currentHistoryId();
         var backView = $ionicHistory.viewHistory().histories[backHistoryId].stack.filter(function (v) {
           return v.viewId === LocalStorage.get("acthViewid");
@@ -162,7 +184,7 @@ angular.module('ActCtrl', [])
   })
 
   /*发起活动*/
-  .controller('newactCtrl', function ($scope, $ionicActionSheet, $http,IP) {
+  .controller('newactCtrl', function ($scope, $ionicActionSheet, $http, IP) {
 
     $http.get("http://localhost:8080/api/acttype")
       .then(function (response) {
@@ -214,7 +236,7 @@ angular.module('ActCtrl', [])
     $scope.save = function () {
       $http({
         method: 'POST',
-        url:IP.info()+ '/api/newact',
+        url: IP.info() + '/api/newact',
         data: $.param($scope.formData)
       }).then(function successCallback(response) {
         console.log(response);
